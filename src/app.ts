f2a3dc5b76040b94a94a2ad81fe6424f7e184b2b
default-profile <105358511+default-profile@users.noreply.github.com>
2024-05-23 16:36:04 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send('(Update 3) Server is up & running...'));
+app.get('/', (req, res) => res.send('(Update 4) Server is up & running...'));
 
 app.get('/health', (req, res) => res.send('Server is healthy!'));
 
